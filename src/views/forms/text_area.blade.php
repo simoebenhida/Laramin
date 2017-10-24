8bbf1f8f36621f4b5870487cfe67b1ec84e3bbed
A-Elwafa <eslamaboelwafa@gmail.com>
2017-10-24 15:31:08 +0200

    Modify id of textarea with label

--- src/views/forms/text_area.blade.php
+++ src/views/forms/text_area.blade.php
@@ -1,5 +1,5 @@
 <div class="field">
           <label for="{{ $name }}" class="label">{{ title_case($name) }}</label>
-            <textarea class="textarea @if($errors->has($name)) is-danger @endif" rows="4" name="{{ $name }}">@if(empty($value)){{ old($name)}}@else{{ $value }}@endif</textarea>
+            <textarea id="{{ $name }}" class="textarea @if($errors->has($name)) is-danger @endif" rows="4" name="{{ $name }}">@if(empty($value)){{ old($name)}}@else{{ $value }}@endif</textarea>
              <p class="help is-danger">{{ $errors->first($name) }}</p>
         </div>
